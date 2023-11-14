582eb7cf29476bd81f92cc339faa2a03310405cb
Vadim Smirnov <smirnou.vadzim@gmail.com>
2023-11-14 10:17:06 +0100

    documentation improvemens

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,37 +5,38 @@ const site = 'https://docs.refact.ai/';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Refact Documentation',
-			logo: {
-				light: '/src/assets/logo-light.svg',
-				dark: '/src/assets/logo-dark.svg',
-				replacesTitle: true,
-			},
-			social: {
-				github: 'https://github.com/smallcloudai',
-				discord: 'https://smallcloud.ai/discord'
-			},
-			head: [
-				{
-					tag: 'meta',
-					attrs: { property: 'og:image', content: site + 'og.jpg' }
-				},
-				{
-					tag: 'meta',
-					attrs: { property: 'twitter:image', content: site + 'og.jpg' }
-				},
-				{
-					tag: 'script',
-					attrs: {
-						async: true,
-						src: 'https://www.googletagmanager.com/gtag/js?id=G-76LB6JQLMK',
-					},
-				},
-				{
-					tag: 'script',
-					content: `
+  integrations: [starlight({
+    title: 'Refact Documentation',
+    logo: {
+      light: '/src/assets/logo-light.svg',
+      dark: '/src/assets/logo-dark.svg',
+      replacesTitle: true
+    },
+    social: {
+      github: 'https://github.com/smallcloudai',
+      discord: 'https://smallcloud.ai/discord'
+    },
+    head: [{
+      tag: 'meta',
+      attrs: {
+        property: 'og:image',
+        content: site + 'og.jpg'
+      }
+    }, {
+      tag: 'meta',
+      attrs: {
+        property: 'twitter:image',
+        content: site + 'og.jpg'
+      }
+    }, {
+      tag: 'script',
+      attrs: {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-76LB6JQLMK'
+      }
+    }, {
+      tag: 'script',
+      content: `
 						window.dataLayer = window.dataLayer || [];
 						function gtag() {
 							dataLayer.push(arguments);
@@ -43,83 +44,78 @@ export default defineConfig({
 						gtag('js', new Date());
 
 						gtag('config', 'G-76LB6JQLMK');
-					`,
-				},
-				{
-					tag: 'script',
-					content: `
+					`
+    }, {
+      tag: 'script',
+      content: `
 					!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
 					posthog.init('phc_dhlxxMkxVCImmYtDh7gwD7y4EUbjus3iGNjaonPsJlO',{api_host:'https://data.smallcloud.ai'})
-					`,
-				},
-			],
-			sidebar: [
-				{
-					label: 'Introduction',
-					items: [
-						{ label: 'Overview', link: '/introduction/overview/' },
-						{
-							label: 'Features',
-							items: [
-								{ label: 'AI Chat', link: '/features/ai-chat/' },
-								{ label: 'Code Completion', link: '/features/code-completion/' },
-								{ label: 'AI Toolbox', link: '/features/ai-toolbox/' },
-								{ label: 'Fine-tuning', link: '/features/fine-tuning/' },
-								{ label: 'Self-hosted Features', link: '/features/self-hosted/' },
-							]
-						},
-					],
-				},
-				{
-					label: 'Plugins',
-					items: [
-						{ label: 'Refact for VS Code', link: '/plugins/vs-code/' },
-						{ label: 'Refact for Jetbrains', link: '/plugins/jetbrains/' },
-					]
-				},
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Self-hosted Refact', link: '/guides/self-hosted/' },
-						{ label: 'Enterprise Refact', link: '/guides/enterprise/' },
-					]
-				},
-				{
-					label: 'Supported Models',
-					items: [
-						{ label: 'Supported Models in Refact', link: '/supported-models/supported-models/' },
-						// { label: 'Refact LLM', link: '/supported-models/refact-llm/' },
-						// { label: 'Starcoder', link: '/supported-models/starcoder/' },
-						// { label: 'Wizard Coder', link: '/supported-models/wizard-coder/' },
-						// { label: 'Code Llama', link: '/supported-models/code-llama/' },
-						// { label: 'Llama2', link: '/supported-models/llama2/' },
-					]
-				},
-				{
-					label: 'FAQ',
-					link: '/faq/',
-				},
-				{
-					label: 'Privacy',
-					link: '/privacy/',
-				},
-				{
-					label: 'Contributing',
-					link: '/contributing/',
-				},
-				{
-					label: 'Changelog',
-					link: '/changelog/',
-				},
-			],
-			customCss: [
-				// Relative path to your custom CSS file
-				'./src/styles/custom.css',
-			],
-			editLink: {
-				baseUrl: 'https://github.com/smallcloudai/web_docs_refact_ai/edit/main/',
-			},
-			lastUpdated: true,
-		}),
-	],
+					`
+    }],
+    sidebar: [{
+      label: 'Introduction',
+      items: [{
+        label: 'Overview',
+        link: '/introduction/overview/'
+      }, {
+        label: 'Features',
+        items: [{
+          label: 'AI Chat',
+          link: '/features/ai-chat/'
+        }, {
+          label: 'Code Completion',
+          link: '/features/code-completion/'
+        }, {
+          label: 'AI Toolbox',
+          link: '/features/ai-toolbox/'
+        }, {
+          label: 'Fine-tuning',
+          link: '/features/fine-tuning/'
+        }, {
+          label: 'Self-hosted Features',
+          link: '/features/self-hosted/'
+        }]
+      }]
+    }, {
+      label: 'Plugins',
+      items: [{
+        label: 'Refact for VS Code',
+        link: '/plugins/vs-code/'
+      }, {
+        label: 'Refact for Jetbrains',
+        link: '/plugins/jetbrains/'
+      }]
+    }, {
+      label: 'Guides',
+      items: [{
+        label: 'Self-hosted Refact',
+        link: '/guides/self-hosted/'
+      }, {
+        label: 'Enterprise Refact',
+        link: '/guides/enterprise/'
+      }]
+    }, {
+      label: 'Supported Models',
+      link: '/supported-models/'
+    }, {
+      label: 'FAQ',
+      link: '/faq/'
+    }, {
+      label: 'Privacy',
+      link: '/privacy/'
+    }, {
+      label: 'Contributing',
+      link: '/contributing/'
+    }, {
+      label: 'Changelog',
+      link: '/changelog/'
+    }],
+    customCss: [
+      // Relative path to your custom CSS file
+      './src/styles/custom.css'],
+    editLink: {
+      baseUrl: 'https://github.com/smallcloudai/web_docs_refact_ai/edit/main/'
+    },
+    lastUpdated: true
+  })]
 });
--- src/content/docs/features/code-completion.md
+++ src/content/docs/features/code-completion.md
@@ -3,4 +3,6 @@ title: Code Completion
 description: A reference page for code completion.
 ---
 
-When you write code in your IDE, Refact can auto-complete the code for you based on the context of your file. We use a technique called “Fill-in-the-middle” to pull the context before and after the cursor. 
+When you write code in your IDE, Refact can auto-complete the code for you based on the context of your file. We use a technique called “Fill-in-the-middle” to pull the context before and after the cursor.
+
+Refact's autocomplete is powered by LLM models. For the cloud version, `Refact-1.6-fim` model is being used for the autocomplete feature. If you are using the self-hosted version, you can pick one of the models available. The complete list of models available can be found in the [Supported Models](https://docs.refact.ai/supported-models/) section.
